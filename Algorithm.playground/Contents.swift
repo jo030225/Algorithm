5b69fb9a597ba6c92e40ac7e257a2f0aa0595705
jo030225 <whwngur123@naver.com>
2021-01-09 01:03:04 +0900

    K번째수 완료

--- Algorithm.playground/Contents.swift
+++ Algorithm.playground/Contents.swift
@@ -1,3 +1,21 @@
 import UIKit
 
-var str = "Hello, playground"
+
+// 프로그래머스 K번째수
+
+func solution(_ array:[Int], _ commands:[[Int]]) -> [Int] {
+    var resultArr = [Int]()
+    
+    for command in commands {
+        let start = command[0] - 1
+        let end = command[1] - 1
+        let target = command[2] - 1
+        let tmp = array[start...end].sorted()
+        resultArr.append(tmp[target])
+    }
+    
+    return resultArr
+}
+
+solution([1,5,2,6,3,7,4], [[2,5,3], [4,4,1], [1,7,3]])
+
