7fc8287deec190b98a0e1b361f9ca0249380e1e9
jo030225 <whwngur123@naver.com>
2021-01-22 00:06:05 +0900

    프로그래머스 내적

--- Algorithm.playground/Contents.swift
+++ Algorithm.playground/Contents.swift
@@ -348,3 +348,16 @@ import UIKit
 //
 //solution("asdfffsfsa")
 
+// 프로그래머스 내적
+
+func solution(_ a:[Int], _ b:[Int]) -> Int {
+    var result = 0
+    
+    for i in 0 ..< a.count {
+        result += a[i] * b[i]
+    }
+    
+    return result
+}
+
+solution([1,2,3,4], [-3,-1,0,2])
