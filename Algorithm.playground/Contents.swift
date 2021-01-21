d9ee6302005de3f01708c0b4087c67329136d534
jo030225 <whwngur123@naver.com>
2021-01-21 23:51:13 +0900

    프로그래머스 문자열 내림차순으로 배치하기

--- Algorithm.playground/Contents.swift
+++ Algorithm.playground/Contents.swift
@@ -342,8 +342,9 @@ import UIKit
 
 // 프로그래머스 문자열 내림차순으로 배치하기
 
-func solution(_ s:String) -> String {
-    return String(Array(s).sorted(by: >))
-}
+//func solution(_ s:String) -> String {
+//    return String(Array(s).sorted(by: >))
+//}
+//
+//solution("asdfffsfsa")
 
-solution("asdfffsfsa")
