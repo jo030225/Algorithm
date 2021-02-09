a56a52f921029ca4cdbfde4f172a0038e0906db1
jo030225 <whwngur123@naver.com>
2021-02-09 23:40:36 +0900

    [완료] 다리를 지나는 트럭

--- Algorithm.playground/Contents.swift
+++ Algorithm.playground/Contents.swift
@@ -694,7 +694,29 @@ import UIKit
 // 프로그래머스 다리를 지나는 트럭
 
 func solution(_ bridge_length:Int, _ weight:Int, _ truck_weights:[Int]) -> Int {
+    var time = 0
+    var wait = truck_weights
+    var w = 0
+    var times = [Int]()
+    var weights = [Int]()
     
-    
-    return 0
+    while !wait.isEmpty{
+        time += 1
+        if !times.isEmpty{
+            if time == times.first! {
+                times.removeFirst()
+                w -= weights.first!
+                weights.removeFirst()
+            }
+        }
+        if w+wait.first! <= weight{
+            w += wait.first!
+            weights.append(wait.first!)
+            times.append(time+bridge_length)
+            wait.removeFirst()
+        }
+    }
+    return times.last!
 }
+
+solution(100, 100, [10,10,10,10,10,10,10,10,10,10])
