ba19dfb1f2f86e15d95c2ee555343e85ea23daec
jo030225 <whwngur123@naver.com>
2021-02-14 01:39:19 +0900

    [푸는 중] 가장 큰 수

--- Algorithm.playground/Contents.swift
+++ Algorithm.playground/Contents.swift
@@ -769,58 +769,120 @@ import UIKit
 
 // 프로그래머스 기능개발
 
-func solution(_ progresses:[Int], _ speeds:[Int]) -> [Int] {
-    var resultArr: [(Int,Int)] = []
-    var resultDay = [Int]()
-    var result = [Int]()
-    
-    for i in 0 ..< progresses.count {
-        resultArr.append((progresses[i],speeds[i]))
-    }
-    
-    for i in 0 ..< resultArr.count {
-        var count = resultArr[i].0
-        for j in 0... {
-            if count < 100 {
-                count += resultArr[i].1
+//func solution(_ progresses:[Int], _ speeds:[Int]) -> [Int] {
+//    var resultArr: [(Int,Int)] = []
+//    var resultDay = [Int]()
+//    var result = [Int]()
+//
+//    for i in 0 ..< progresses.count {
+//        resultArr.append((progresses[i],speeds[i]))
+//    }
+//
+//    for i in 0 ..< resultArr.count {
+//        var count = resultArr[i].0
+//        for j in 0... {
+//            if count < 100 {
+//                count += resultArr[i].1
+//            } else {
+//                resultDay.append(j)
+//                break
+//            }
+//        }
+//    }
+//
+//    print(resultDay)
+//
+//    for i in 0 ..< resultDay.count - 1 {
+//        if resultDay[i] > resultDay[i + 1] {
+//            resultDay[i + 1] = resultDay[i]
+//        }
+//    }
+//
+//    print(resultDay)
+//
+//    var count = 1
+//    for i in 0 ..< resultDay.count - 1 {
+//        if resultDay[i] != resultDay[i + 1] {
+//            result.append(count)
+//            count = 1
+//        } else {
+//            count += 1
+//            if i == resultDay.count - 2 {
+//                result.append(count)
+//                break
+//            }
+//        }
+//        if i == resultDay.count - 2 {
+//            count = 1
+//            result.append(count)
+//        }
+//    }
+//
+//    print(result)
+//
+//    return result
+//}
+//
+//solution([93, 30, 55], [1, 30, 5])
+
+// 프로그래머스 가장 큰 수
+
+func solution(_ numbers:[Int]) -> String {
+    var number = numbers
+    var resultArr = [Int]()
+    var max = 0
+    var realmax = 0
+    var count = 0
+    var a = 0
+
+    while !(number.isEmpty) {
+
+        for i in 0 ..< number.count {
+            if number[a] == 1000 {
+                continue
+            } else if number[a] >= 100 {
+                if realmax < number[a] / 100 {
+                    max = number[a]
+                    realmax = number[a] / 100
+                }
+            } else if number[a] >= 10 {
+                if realmax < number[a] / 10 {
+                    max = number[a]
+                    realmax = number[a] / 10
+                }
             } else {
-                resultDay.append(j)
+                if realmax <= number[a] {
+                    max = number[a]
+                    realmax = number[a]
+                }
+            }
+            print("number: \(number)")
+            print("a: \(a)")
+            print("max: \(max)")
+            print("realmax: \(realmax)")
+            a += 1
+        }
+        resultArr.append(max)
+        print("resultArr: \(resultArr)\n")
+        for i in 0 ..< number.count {
+            if number[i] == max {
+                number.remove(at: i)
                 break
             }
         }
+        max = 0
+        realmax = 0
+        a = 0
+        print("a: \(a)")
+        print("max: \(max)")
+        print("realmax: \(realmax)")
+        count += 1
+       
     }
-    
-    print(resultDay)
-    
-    for i in 0 ..< resultDay.count - 1 {
-        if resultDay[i] > resultDay[i + 1] {
-            resultDay[i + 1] = resultDay[i]
-        }
-    }
-    
-    print(resultDay)
-    
-    var count = 1
-    for i in 0 ..< resultDay.count - 1 {
-        if resultDay[i] != resultDay[i + 1] {
-            result.append(count)
-            count = 1
-        } else {
-            count += 1
-            if i == resultDay.count - 2 {
-                result.append(count)
-                break
-            }
-        }
-        if i == resultDay.count - 2 {
-            count = 1
-            result.append(count)
-        }
-    }
-    
-    print(result)
-    
-    return result
+
+    print(resultArr)
+
+    return ""
 }
 
-solution([93, 30, 55], [1, 30, 5])
+solution([3, 40, 64, 5, 9])
