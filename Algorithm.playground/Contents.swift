43fab31033979505276b9ec01067581fd4fed6cf
jo030225 <whwngur123@naver.com>
2021-04-15 20:49:17 +0900

    [ADD] 피보나치 수

--- Algorithm.playground/Contents.swift
+++ Algorithm.playground/Contents.swift
@@ -950,39 +950,61 @@ import UIKit
 
 // 체육복
 
-func solution(_ n:Int, _ lost:[Int], _ reserve:[Int]) -> Int {
-    
-    // 가지고 있는 체육복 개수 저장 (기본적으로 1벌)
-    var students = [Int](repeating: 1, count: n)
-    
-    // 잃어버린 사람은 체육복 0벌
-    for l in lost {
-        students[l-1] = 0
-    }
-    // 여벌 가져 온 사람은 +1 벌
-    for r in reserve {
-        students[r-1] += 1
-    }
-    
-    var count = 0 // 체육복을 빌리지 못한 학생 수
-    for i in 0..<n {
-        if students[i] == 0 {
-            if i>0 && students[i-1] > 1 {
-                // 앞번호 학생에게 빌린다.
-                students[i] = 1
-                students[i-1] = 1
-            } else if i<n-1 && students[i+1] > 1 {
-                // 뒷번호 학생에게 빌린다.
-                students[i] = 1
-                students[i+1] = 1
-            } else {
-                // 빌리지 못했다.
-                count += 1
-            }
-        }
-    }
-    
-    return n - count
-}
+//func solution(_ n:Int, _ lost:[Int], _ reserve:[Int]) -> Int {
+//
+//    // 가지고 있는 체육복 개수 저장 (기본적으로 1벌)
+//    var students = [Int](repeating: 1, count: n)
+//
+//    // 잃어버린 사람은 체육복 0벌
+//    for l in lost {
+//        students[l-1] = 0
+//    }
+//    // 여벌 가져 온 사람은 +1 벌
+//    for r in reserve {
+//        students[r-1] += 1
+//    }
+//
+//    print(students)
+//
+//    var count = 0 // 체육복을 빌리지 못한 학생 수
+//    for i in 0..<n {
+//        if students[i] == 0 {
+//            if i>0 && students[i-1] > 1 {
+//                // 앞번호 학생에게 빌린다.
+//                students[i] = 1
+//                students[i-1] = 1
+//            } else if i<n-1 && students[i+1] > 1 {
+//                // 뒷번호 학생에게 빌린다.
+//                students[i] = 1
+//                students[i+1] = 1
+//            } else {
+//                // 빌리지 못했다.
+//                count += 1
+//            }
+//        }
+//    }
+//
+//    return n - count
+//}
+//
+//solution(5, [2,4], [1,3,5])
 
-solution(3, [3], [1, 3])
+// 피보나치 수
+
+//func solution(_ n:Int) -> Int {
+//    var a = [Int]()
+//
+//    for i in 0 ... n {
+//        if i <= 1 {
+//            a.append(i)
+//        } else {
+//            a.append((a[i - 2] + a[i - 1]) % 1234567)
+//        }
+//    }
+//    print(a)
+//
+//    return a[n]
+//}
+//
+//solution(5)
+
